28590190f82f6324ebce43d40044df1f207bbb4f
0m3ga13 <barkaoussama13@gmail.com>
2023-07-21 22:04:36 +0100

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+
+const saturdayFun = (activity = "roller-skate") => `This Saturday, I want to ${activity}!`;
+
+const mondayWork = (activity = "go to the office") => `This Monday, I will ${activity}.`;
+
+const wrapAdjective = (whatever = "*") =>  (however = "special") => `You are ${whatever}${however}${whatever}!`
